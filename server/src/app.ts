409404574a83e9097012df065d81e1cc4e17c053
yishaq-official <yishaq.official@gmail.com>
2026-08-13 06:18:57 +0300

    register heritage routes under the /api/heritages endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import type {Request, Response} from "express";
 import dotenv from "dotenv";
+import heritageRoutes from "./routes/heritageRoutes.js";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ const app = express();
 app.use(cors());
 app.all("/api/auth/*", toNodeHandler(auth));
 app.use(express.json());
+
+app.use("/api/heritages", heritageRoutes);
 
 app.get("/api/hello", (req: Request, res: Response) => {
     res.send("Hello World");
