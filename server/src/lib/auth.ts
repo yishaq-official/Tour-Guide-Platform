73a2b20c7a363bca7d5b04d5f7f267903f19b53b
yishaq-official <yishaq.official@gmail.com>
2026-08-17 18:15:13 +0300

    configure Google OAuth provider on server and initialize auth client on frontend

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -14,8 +14,14 @@ export const auth = betterAuth({
     database: mongodbAdapter(db, {
         client
     }),
-    EmailAndPassword:{
+    emailAndPassword:{
         enabled: true,
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }
     },
     trustedOrigins: [
         "http://localhost:5173",
